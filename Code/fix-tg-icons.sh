362ef40b88a9fb9496a443013459e890ab044c4b
Andy Kluger <andykluger@gmail.com>
2017-01-16 21:31:09 -0500

    graphicsmagick for saner command-subcommand pattern

--- Code/fix-tg-icons.sh
+++ Code/fix-tg-icons.sh
@@ -4,29 +4,32 @@ cd ~/.TelegramDesktop/tdata/ticons
 rm -r *
 
 # blue -> grey
-# convert /usr/share/pixmaps/telegram.png -modulate 51,0,100 ./ico_22.png
+#gm convert /usr/share/pixmaps/telegram.png -modulate 51,0,100 ./ico_22.png
 # or not:
 cp /usr/share/pixmaps/telegram.png ./ico_22.png
 
+# invert and dim, for dark themes
+#gm convert ico_22.png -negate ico_22.png
+#gm convert ico_22.png -modulate 95,100,100 ico_22.png
 
 # white center -> transparent center (base):
-# convert ico_22.png -fuzz 40% -transparent white ico_22.png
+# gm convert ico_22.png -fuzz 40% -transparent white ico_22.png
 # or not.
 
 # blue -> orangered (unmuted, messages):
-convert /usr/share/pixmaps/telegram.png -modulate 100,120,0 ico_22_1.png
+gm convert /usr/share/pixmaps/telegram.png -modulate 100,120,0 ico_22_1.png
 # or not:
 #cp /usr/share/pixmaps/telegram.png ico_22_1.png
 
 # blue -> dark blue (muted, no messages):
-# convert ico_22.png -modulate 50,100,100 icomute_22_0.png
+# gm convert ico_22.png -modulate 50,100,100 icomute_22_0.png
 # or not:
 #cp /usr/share/pixmaps/telegram.png icomute_22_0.png
-#convert /usr/share/pixmaps/telegram.png -modulate 60,0,100 icomute_22_0.png
+#gm convert /usr/share/pixmaps/telegram.png -modulate 60,0,100 icomute_22_0.png
 cp ico_22.png icomute_22_0.png
 
 # orangered -> dark orangered (muted, messages):
-convert ico_22_1.png -modulate 50,100,100 icomute_22_1.png
+gm convert ico_22_1.png -modulate 50,100,100 icomute_22_1.png
 # or not:
 #cp /usr/share/pixmaps/telegram.png icomute_22_1.png
 

5c0d46245fb979a03442707ef5cf31026a05636e
Andy Kluger <andykluger@gmail.com>
2017-10-19 19:04:07 -0400

    check last.fm now playing status if nothing found locally

--- Code/kargos/now_playing/now_playing.5s.py
+++ Code/kargos/now_playing/now_playing.5s.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python3
+from contextlib import suppress
+
 from plumbum.cmd import playerctl
 from plumbum import ProcessExecutionError
+from requests import get
+
+from vault import LASTFM_API_KEY, LASTFM_USER
 
 
 fmt = " | font='Iosevka Light' size=21"
 # event calendar clock height 34px
 
-get = playerctl['metadata']
+
+def now_playing(api_key=LASTFM_API_KEY, user=LASTFM_USER):
+    try:
+        return {detail: playerctl['metadata'](detail) for detail in ('title', 'artist', 'album')}
+    except ProcessExecutionError:
+        base = 'http://ws.audioscrobbler.com/2.0/'
+        r = get(base, params={
+            'format': 'json', 'api_key': api_key,
+            'method': 'user.getRecentTracks',
+            'user': user
+        })
+        if r.ok:
+            track = r.json()['recenttracks']['track'][0]
+            with suppress(KeyError):
+                if track['@attr']['nowplaying']:
+                    return {
+                        'title': track['name'],
+                        'artist': track['artist']['#text'],
+                        'album': track['album']['#text']
+                    }
+
 
 def resize(txt, size, pre='» ', post=' «', ellipsis='center'):
     free = size - len(txt)
@@ -19,16 +44,19 @@ def resize(txt, size, pre='» ', post=' «', ellipsis='center'):
         a, b = divmod(size - 1, 2)
         return f"{pre}{txt[:a + b]}…{txt[-a:]}{post}"
 
-try:
-    title, artist = get('title'), get('artist')
-except ProcessExecutionError:
-    print(f"{fmt} iconName=spotify-indicator bash=spotify onclick=bash")
-else:
-    # size = len(artist)
-    size = min(15, max(len(artist), len(title)))
-    print(f"{resize(artist, size)}{fmt}")
-    print(f"{resize(title,  size)}{fmt}")
-    print('---')
-    print(f"{title}       {fmt}", "iconName=media-album-track")
-    print(f"{artist}      {fmt}", "iconName=view-media-artist")
-    print(f"{get('album')}{fmt}", "iconName=media-album-cover")
+
+if __name__ == '__main__':
+    details = now_playing()
+    try:
+        title, artist, album = details['title'], details['artist'], details['album']
+    except TypeError:
+        print(f"{fmt} iconName=spotify-indicator bash=spotify onclick=bash")
+    else:
+        # size = len(artist)
+        size = min(15, max(len(artist), len(title)))
+        print(f"{resize(artist, size)}{fmt}")
+        print(f"{resize(title,  size)}{fmt}")
+        print('---')
+        print(f"{title} {fmt}", "iconName=media-album-track")
+        print(f"{artist}{fmt}", "iconName=view-media-artist")
+        print(f"{album} {fmt}", "iconName=media-album-cover")

14c6513c84fc449e3eefc153e511f1dd90b87f17
Andy Kluger <andykluger@gmail.com>
2017-10-19 19:07:33 -0400

    playdelete now takes dirs to walk through, uses a file extension whitelist, quits more gracefully, and shuffles the vids

--- Code/playdelete.py
+++ Code/playdelete.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
-from sys import argv
+from sys import argv, exit
+from random import shuffle
 
-from plumbum import local
+from plumbum import local, ProcessExecutionError
 from plumbum.cmd import mpv
 from plumbum.cli.terminal import choose
 
 
 if __name__ == '__main__':
-    for mov in argv[1:]:
+    vids, exts = [], ('mkv', 'mp4', 'flv', 'avi', 'm4v', 'wmv')  # maybe use mimetypes instead
+    for path in argv[1:]:
+        root = local.path(path)
+        vids.extend(root.walk(lambda f: f.lower().rsplit('.', 1)[-1] in exts))
+    shuffle(vids)
+    for vid in vids:
         action = "replay"
         while action == "replay":
-            mpv(mov)
-            action = choose(
-                f"What should we do with {mov}?",
-                ["let it be", "kill it dead", "replay"],
-                "let it be"
-            )
+            try:
+                mpv(vid)
+            except ProcessExecutionError as e:
+                print(e)
+                break
+            try:
+                action = choose(
+                    f"What should we do with {vid}? (Ctrl-c to quit)",
+                    ["let it be", "kill it dead", "replay"],
+                    "let it be"
+                )
+            except KeyboardInterrupt:
+                exit(0)
         if action == "kill it dead":
-            local.path(mov).delete()
+            vid.delete()
             print("SPLAT!")

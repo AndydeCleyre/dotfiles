7c66a83a97ddc397f7c2bcc2454e78d7ad4dc592
Andy Kluger <andykluger@gmail.com>
2017-11-16 11:44:38 -0500

    dash price for kargos

---
+++ Code/kargos/dash/dash.300s.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from time import sleep
+
+from requests import get
+from requests.exceptions import ConnectionError
+
+fmt = " | font='Iosevka'"
+API_BASE = 'https://min-api.cryptocompare.com'
+while True:
+    try:
+        r = get(f"{API_BASE}/data/price", params={'fsym': 'DASH', 'tsyms': 'USD'})
+    except ConnectionError:
+        sleep(10)
+    else:
+        break
+price = r.json()['USD']
+print(f"${price:.2f}{fmt}")

ed3d13c4b7c4e46b7af63b6b123db1b002f9a452
Andy Kluger <andykluger@gmail.com>
2018-05-16 14:55:28 -0400

    font adjustment

--- Code/kargos/dash/dash.300s.py
+++ Code/kargos/dash/dash.300s.py
@@ -4,7 +4,7 @@ from time import sleep
 from requests import get
 from requests.exceptions import ConnectionError
 
-fmt = " | font='Iosevka'"
+fmt = " | font='Iosevka Custom' size=15"
 API_BASE = 'https://min-api.cryptocompare.com'
 while True:
     try:

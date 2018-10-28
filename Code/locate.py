eff950dd6790f5de8a93a30cb36ffbd467d30ffd
Andy Kluger <andykluger@gmail.com>
2018-10-28 14:45:20 -0400

    more graceful aborts for locate.py

--- Code/locate.py
+++ Code/locate.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-from sys import argv
+from sys import argv, exit
 from contextlib import suppress
 from plumbum.cmd import locate, grep
-from plumbum import FG, ProcessExecutionError
+from plumbum import ProcessExecutionError
 
 cmd = locate['-i', argv[1]]
 for term in argv[2:]:
     cmd = (cmd | grep['-i', term])
 with suppress(ProcessExecutionError):
-    cmd & FG
+    try:
+        cmd.run_fg()
+    except KeyboardInterrupt:
+        print("\nSearch Canceled")
+        exit(1)
 

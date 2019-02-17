643b724a8c38d95057bfca20c9bf2ba63866b254
Andy Kluger <andykluger@gmail.com>
2019-02-17 15:15:21 -0500

    use micro and konsole instead of sublime for txtnamer

--- Code/txtnamer.py
+++ Code/txtnamer.py
@@ -6,20 +6,20 @@ from sys import argv
 
 from plumbum import local
 from plumbum.colors import red, green
-from plumbum.cmd import subl
+from plumbum.cmd import konsole
 
 
-old_names = argv[1:]
+edit = konsole['-e', 'micro']
+old_names = argv[1:] or local.path().list()
 with local.tempdir() as tmp:
     name_file = tmp / 'txtnamer_names'
     name_file.write('\n'.join(old_names), 'utf8')
-    subl('-w', name_file)
+    edit(name_file)
     new_names = name_file.read('utf8').strip().splitlines()
 if len(new_names) == len(old_names):
     for old_path, new_path in zip(
-        map(local.path, old_names),
-        map(local.path, new_names)
+        map(local.path, old_names), map(local.path, new_names)
     ):
         if old_path != new_path:
-            print(red | old_path.name, green | new_path.name, '', sep='\n')
+            print(old_path.name | red, new_path.name | green, '', sep='\n')
             old_path.move(new_path)

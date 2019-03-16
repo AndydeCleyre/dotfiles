4c0a23b035e0486e9f8e2b177ff84941da6d1a9e
Andy Kluger <andykluger@gmail.com>
2019-03-16 00:06:54 -0400

    cool-retro-term > konsole for terminal-app launching

--- Code/stream.sh
+++ Code/stream.sh
@@ -1,5 +1,7 @@
 #!/bin/execlineb -S0
-konsole --new-tab -e /home/andy/Code/stream.py $@
+# konsole --new-tab -e /home/andy/Code/stream.py $@
+cool-retro-term -T "stream.py" -e /home/andy/Code/stream.py $@
+
 #!/bin/sh
 # exec konsole --new-tab -e /home/andy/Code/stream.py $@
 

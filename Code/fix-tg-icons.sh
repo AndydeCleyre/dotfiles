d732703815e6802f90ce03506f810de930b520c4
Andy Kluger <andykluger@gmail.com>
2019-04-12 01:14:50 -0400

    improve telegram tray icon, starting with papirus' svgs

--- Code/fix-tg-icons.sh
+++ Code/fix-tg-icons.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
+set -e
 
-cd ~/.TelegramDesktop/tdata/ticons
-rm -r *
+cd ~/.local/share/TelegramDesktop/tdata/ticons
+rm -rf *.png
 
-# blue -> grey
-#gm convert /usr/share/pixmaps/telegram.png -modulate 51,0,100 ./ico_22.png
-# or not:
-cp /usr/share/pixmaps/telegram.png ./ico_22.png
+cp /usr/share/icons/Papirus/24x24/panel/telegram-* ./
+sed -i 's/dfdfdf/c2b790/' *.svg
+sed -i 's/4285f4/a94d37/' *.svg
+sed -i 's/c2b790/a94d37/' telegram-attention-panel.svg
 
-# invert and dim, for dark themes
-#gm convert ico_22.png -negate ico_22.png
-#gm convert ico_22.png -modulate 95,100,100 ico_22.png
+inkscape -z -e icon_22_0.png -w 256 -h 256 telegram-panel.svg
+inkscape -z -e icon_22_1.png -w 256 -h 256 telegram-attention-panel.svg
+ln -s icon_22_0.png iconmute_22_0.png
+inkscape -z -e iconmute_22_1.png -w 256 -h 256 telegram-mute-panel.svg
+rm -rf *.svg
 
-# white center -> transparent center (base):
-# gm convert ico_22.png -fuzz 40% -transparent white ico_22.png
-# or not.
-
-# blue -> orangered (unmuted, messages):
-gm convert /usr/share/pixmaps/telegram.png -modulate 100,120,0 ico_22_1.png
-# or not:
-#cp /usr/share/pixmaps/telegram.png ico_22_1.png
-
-# blue -> dark blue (muted, no messages):
-# gm convert ico_22.png -modulate 50,100,100 icomute_22_0.png
-# or not:
-#cp /usr/share/pixmaps/telegram.png icomute_22_0.png
-#gm convert /usr/share/pixmaps/telegram.png -modulate 60,0,100 icomute_22_0.png
-cp ico_22.png icomute_22_0.png
-
-# orangered -> dark orangered (muted, messages):
-gm convert ico_22_1.png -modulate 50,100,100 icomute_22_1.png
-# or not:
-#cp /usr/share/pixmaps/telegram.png icomute_22_1.png
-
-for f in {2..2000}; do ln -s ico_22_1.png ico_22_$f.png; done
-for f in {2..2000}; do ln -s icomute_22_1.png icomute_22_$f.png; done
+for f in {2..2000}; do ln -s icon_22_1.png icon_22_$f.png; done
+for f in {2..2000}; do ln -s iconmute_22_1.png iconmute_22_$f.png; done

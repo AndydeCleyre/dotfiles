7fea03f75ef41c39b667cf621bad012b4c162bc8
Andy Kluger <andykluger@gmail.com>
2019-04-13 13:30:15 -0400

    un-hardcode colors; read from current scheme

--- Code/fix-tg-icons.sh
+++ Code/fix-tg-icons.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
+plain_color=`python3 -c "print(''.join(hex(int(i))[2:] for i in '$(kreadconfig5 --group Colors:Window --key ForegroundNormal)'.split(',')))"`
+alert_color=`python3 -c "print(''.join(hex(int(i))[2:] for i in '$(kreadconfig5 --group Colors:Window --key DecorationFocus)'.split(',')))"`
+
 cd ~/.local/share/TelegramDesktop/tdata/ticons
 rm -rf *.png
 
 cp /usr/share/icons/Papirus/24x24/panel/telegram-* ./
-sed -i 's/dfdfdf/c2b790/' *.svg
-sed -i 's/4285f4/a94d37/' *.svg
-sed -i 's/c2b790/a94d37/' telegram-attention-panel.svg
+sed -i "s/dfdfdf/$plain_color/" *.svg
+sed -i "s/4285f4/$alert_color/" *.svg
+sed -i "s/$plain_color/$alert_color/" telegram-attention-panel.svg
 
 inkscape -z -e icon_22_0.png -w 256 -h 256 telegram-panel.svg
 inkscape -z -e icon_22_1.png -w 256 -h 256 telegram-attention-panel.svg

897d981e81dd30a5d74ffef4da4ff0d70a870e5d
Andy Kluger <andykluger@gmail.com>
2019-04-19 17:24:22 -0400

    retry for crypto price only 3 times; placeholder no-op colorize func

--- Code/panel_scripts/dash/price.py
+++ Code/panel_scripts/dash/price.py
@@ -6,16 +6,21 @@ from requests import get
 from requests.exceptions import ConnectionError
 
 
+def colorize(text, colorhex='6d948d'):
+    return text
+    # return f'<font color="#{colorhex}">{text}</font>'
+
+
 if len(argv) > 1:
-    print("🙈")
+    print(colorize("---"))
 else:
     API_BASE = 'https://min-api.cryptocompare.com'
-    while True:
+    for attempt in range(3):
         try:
             r = get(f"{API_BASE}/data/price", params={'fsym': 'DASH', 'tsyms': 'USD'})
         except ConnectionError:
-            sleep(10)
+            sleep(6)
         else:
+            price = r.json()['USD']
+            print(colorize(f"${price:.2f}"))
             break
-    price = r.json()['USD']
-    print('{', f"${price:.2f}", '}')

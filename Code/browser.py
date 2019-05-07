828f3cbd518863249fc0282a2c4e81383ee4b053
Andy Kluger <andykluger@gmail.com>
2019-05-07 11:59:14 -0400

    gwenview for images; parse imgur albums -> gwenview; lynx accept cookies

--- Code/browser.py
+++ Code/browser.py
@@ -1,23 +1,48 @@
 #!/usr/bin/env python3
+# import re
 import sys
 
-from plumbum import local
-from plumbum.cmd import xclip, mpv, firefox, lynx, rtv
+from bs4 import BeautifulSoup
+from plumbum import local, FG, BG
+from plumbum.cmd import (
+    firefox,
+    gwenview,
+    lynx,
+    mpv,
+    rtv,
+    xclip,
+)
+from requests import get
 
 
-uris = sys.argv[1:] or xclip('-sel', 'clip', '-o').splitlines()
-for uri in uris:
-    if any((
-        uri.startswith('magnet:?xt=urn:btih:'),
-        uri.endswith('.torrent'),
-        '://www.youtube.com/watch' in uri,
-    )):
-        local['~/Code/stream.sh'](uri)
-    elif sys.stdin.isatty():
-        if uri.startswith('https://www.reddit.com/r/'):
-            rtv[uri].run_fg()
+def route(*uris):
+    for uri in uris:
+        if any((
+            uri.startswith('magnet:?xt=urn:btih:'),
+            uri.endswith('.torrent'),
+            '://www.youtube.com/watch' in uri,
+        )):
+            local['~/Code/stream.sh'](uri)
+        elif any((
+            uri.startswith('https://i.imgur.com/'),
+            uri.startswith('https://i.redd.it/'),
+        )):
+            gwenview[uri] & BG(stderr=sys.stderr)
+        elif uri.startswith('https://imgur.com/a/'):
+            r = get(uri)
+            soup = BeautifulSoup(r.text)
+            for img in soup.find_all(attrs={'class': 'post-image-container'}):
+                route(f"https://i.imgur.com/{img.attrs['id']}.jpg")
+        elif sys.stdin.isatty():
+            if uri.startswith('https://www.reddit.com/r/'):
+                rtv[uri] & FG
+            else:
+                lynx['-accept_all_cookies', uri] & FG
         else:
-            lynx[uri].run_fg()
-    else:
-        with local.env(GTK_USE_PORTAL=1):
-            firefox[uri].run_bg()
+            with local.env(GTK_USE_PORTAL=1):
+                firefox[uri] & BG(stderr=sys.stderr)
+
+
+if __name__ == '__main__':
+    uris = sys.argv[1:] or xclip('-sel', 'clip', '-o').splitlines()
+    route(*uris)

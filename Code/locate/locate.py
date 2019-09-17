8096f0b59ae60761144bd06692363e3e37e73da5
Andy Kluger <andykluger@gmail.com>
2019-09-17 01:21:06 -0400

    isolate locate venv

--- Code/locate/locate.py
+++ Code/locate/locate.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from sys import argv, exit
 from contextlib import suppress
+
 from plumbum.cmd import locate, grep
 from plumbum import ProcessExecutionError
+
 
 cmd = locate['-i', argv[1]]
 for term in argv[2:]:

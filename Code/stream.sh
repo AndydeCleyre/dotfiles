6cc3b6ca94755e42da89edba1459b9e88b1e2966
Andy Kluger <andykluger@gmail.com>
2019-09-17 01:06:48 -0400

    isolate stream venv

--- Code/stream.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/execlineb -S0
-# konsole --new-tab -e /home/andy/Code/stream.py $@
-cool-retro-term -T "stream.py" -e /home/andy/Code/stream.py $@
-
-#!/bin/sh
-# exec konsole --new-tab -e /home/andy/Code/stream.py $@
-
---
+++ Code/stream/stream.sh
@@ -0,0 +1,8 @@
+#!/bin/execlineb -S0
+# konsole --new-tab -e /home/andy/Code/stream.py $@
+cool-retro-term -T "stream.py" -e /home/andy/Code/stream/stream.py $@
+# konsole --hold -e /home/andy/Code/stream/stream.py $@
+
+#!/bin/sh
+# exec konsole --new-tab -e /home/andy/Code/stream/stream.py $@
+

ac24a2de289b939bbfbf25a92985eeaacc2e3ed9
Andy Kluger <andydecleyre@gmail.com>
2019-10-05 23:57:11 -0400

    structure browser.py; eliminate browser.sh

--- Code/browser.py
+++ Code/browser.py
@@ -3,7 +3,9 @@
 import re
 import sys
 from configparser import ConfigParser
+from dataclasses import dataclass
 from shlex import split
+from typing import Callable, Any
 
 from bs4 import BeautifulSoup
 from plumbum import local, FG, BG
@@ -25,48 +27,98 @@ def get_ice_cmd(appname):
     return local[parts[0]][parts[1:-1]]
 
 
+def get_imgur_album_images(uri):
+    r = get(uri)
+    soup = BeautifulSoup(r.text)
+    return [
+        f"https://i.imgur.com/{img.attrs['id']}.jpg"
+        for img in soup.find_all(
+            attrs={'class': 'post-image-container'}
+        )
+    ]
+
+
+@dataclass
+class Browser:
+    notice: str
+    match: Callable[[str], bool]
+    run: Callable[[str], Any]
+
+
+streamer = Browser(
+    "Attempting to stream . . .",
+    lambda uri: any((
+        uri.endswith('.torrent'),
+        uri.startswith('magnet:?xt=urn:btih:'),
+        re.match(r'^https?://(www\.)?(m\.)?(youtube|vrv)\.com?/(watch|embed)', uri),
+        re.match(r'^https?://youtu\.be/', uri),
+        re.match(r'^https?://(.+\.)?gfycat\.com/.+\.webm', uri),
+        re.match(r'^https?://i\.imgur\.com/', uri) and uri.endswith('v.jpg'),
+        uri.startswith('https://v.redd.it/'),
+    )),
+    lambda uri: local['~/Code/stream.sh'](uri)
+)
+
+
+imager = Browser(
+    "Opening image . . .",
+    lambda uri: any((
+        uri.endswith('.jpg'),
+        uri.startswith('https://i.imgur.com/'),
+        uri.startswith('https://i.redd.it/'),
+    )),
+    lambda uri: gwenview[uri] & BG(stderr=sys.stderr)
+)
+
+
+imgur_splitter = Browser(
+    "Splitting imgur album . . .",
+    lambda uri: re.match(r'^https://imgur\.com/(a|gallery)/', uri),
+    lambda uri: route(*get_imgur_album_images(uri))
+)
+
+
+gmaps = Browser(
+    "Opening map . . .",
+    lambda uri: uri.startswith('https://www.google.com/maps/'),
+    lambda uri: get_ice_cmd('googlemaps')[uri] & BG(stderr=sys.stderr)
+)
+
+
+gphotos = Browser(
+    "Opening photos . . .",
+    lambda uri: any((
+        uri.startswith('https://photos.google.com/'),
+        uri.startswith('https://photos.app.goo.gl/')
+    )),
+    lambda uri: get_ice_cmd('googlephotos')[uri] & BG(stderr=sys.stderr)
+)
+
+
+ttyreddit = Browser(
+    "Using rtv in existing TTY . . .",
+    lambda uri: uri.startswith('https://www.reddit.com/r/') and sys.stdin.isatty() and local.env.get('_') not in ('/bin/rtv',),
+    lambda uri: rtv[uri] & FG
+)
+
+
+ttyweb = Browser(
+    "Using lynx in existing TTY . . .",
+    lambda uri: sys.stdin.isatty() and local.env.get('_') not in ('/bin/rtv',),
+    lambda uri: lynx['-accept_all_cookies', uri] & FG
+)
+
+
 def route(*uris):
     for uri in uris:
-        uri = re.sub(r'^browser:/?/?', '', uri)
-        if any((
-            re.match(r'^https?://(.+\.)?gfycat\.com/.+\.webm', uri),
-            re.match(r'^https?://(www\.)?(youtube|vrv)\.com?/(watch|embed)', uri),
-            re.match(r'^https?://i\.imgur\.com/', uri) and uri.endswith('v.jpg'),
-            uri.endswith('.torrent'),
-            uri.startswith('magnet:?xt=urn:btih:'),
-            # v.reddit
-        )):
-            notify_send('-a', "browser.py", "Attempting to stream . . .")
-            local['~/Code/stream.sh'](uri)
-        elif any((
-            uri.endswith('.jpg'),
-            uri.startswith('https://i.imgur.com/'),
-            uri.startswith('https://i.redd.it/'),
-        )):
-            notify_send('-a', "browser.py", "Opening image . . .")
-            gwenview[uri] & BG(stderr=sys.stderr)
-        elif re.match(r'^https://imgur\.com/(a|gallery)/', uri):
-            notify_send('-a', "browser.py", "Splitting imgur album . . .")
-            r = get(uri)
-            soup = BeautifulSoup(r.text)
-            for img in soup.find_all(attrs={'class': 'post-image-container'}):
-                route(f"https://i.imgur.com/{img.attrs['id']}.jpg")
-        elif uri.startswith('https://www.google.com/maps/'):
-            get_ice_cmd('googlemaps')[uri] & BG(stderr=sys.stderr)
-        elif any((
-            uri.startswith('https://photos.google.com/'),
-            uri.startswith('https://photos.app.goo.gl/')
-        )):
-            get_ice_cmd('googlephotos')[uri] & BG(stderr=sys.stderr)
-        elif sys.stdin.isatty() and local.env.get('_') not in ('/bin/rtv',):
-            if uri.startswith('https://www.reddit.com/r/'):
-                notify_send('-a', "browser.py", "Using rtv in existing TTY . . .")
-                rtv[uri] & FG
-            else:
-                notify_send('-a', "browser.py", "Using lynx in existing TTY . . .")
-                lynx['-accept_all_cookies', uri] & FG
+        uri = re.sub(r'^browser:/{0,2}', '', uri)
+        for browser in (streamer, imager, imgur_splitter, gmaps, gphotos, ttyreddit, ttyweb):
+            if browser.match(uri):
+                notify_send('-a', "browser.py", browser.notice, uri)
+                browser.run(uri)
+                break
         else:
-            notify_send('-a', "browser.py", "Using Firefox . . .")
+            notify_send('-a', "browser.py", "Using Firefox . . .", uri)
             with local.env(GTK_USE_PORTAL=1):
                 firefox[uri] & BG(stderr=sys.stderr)
 

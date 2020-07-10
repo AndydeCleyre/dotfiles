00e4b85044e9ba3b96d6f725cffcf32e634c45dc
Andy Kluger <andykluger@gmail.com>
2020-07-10 13:49:50 -0400

    no longer useful: grenadine

--- Code/grenadine/read.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-TAB_DIR="$HOME/.config/ff-tabs"
-mkdir -p "$TAB_DIR"
-cat "$TAB_DIR"/*.txt | xclip -sel clip
-notify-send -i firefox "Copied $(xclip -sel clip -o | wc -l) URLs from file:" "$(xclip -sel clip -o | head)\n. . ."

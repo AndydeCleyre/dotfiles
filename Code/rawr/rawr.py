aabf94fc43afad1db84c6d6785d2770fb1955a5c
Andy Kluger <andykluger@gmail.com>
2020-07-27 14:12:10 -0400

    improve rawr

--- Code/rawr/rawr.py
+++ Code/rawr/rawr.py
@@ -5,7 +5,7 @@ from time import sleep
 
 from plumbum import FG, local, CommandNotFound
 from plumbum.cli.terminal import choose, ask
-from plumbum.cmd import aria2c
+from plumbum.cmd import aria2c, grep, df
 from plumbum.colors import blue, green, magenta, yellow
 from rarbgapi import RarbgAPI
 
@@ -16,56 +16,49 @@ def mk_search_args():
     except ValueError:
         search_args = {}
     else:
-        search_args = {'category': RarbgAPI.CATEGORY_ADULT}
+        search_args = {'categories': [RarbgAPI.CATEGORY_ADULT]}
     search_args.update({
         'search_string': ' '.join(sys.argv[1:]),
-        'format_': 'json_extended'
+        'extended_response': True
     })
     return search_args
 
 
 def get_results(search_args):
     results = None
-    max_attempts = 3
-    attempts = max_attempts
-    while attempts and not results:
-        if attempts < max_attempts:
-            print(f"Retrying . . .")
-            sleep(1)
-        results = {
-            ' '.join((
-                r.filename,
-                str(r._raw['seeders']) | green,
-                str(r._raw['leechers']) | yellow,
-                f"{r._raw['size'] / 1024**3:.2f} GiB" | blue,
-                r._raw['pubdate'].split()[0] | magenta
-            )): r.download
-            for r in RarbgAPI().search(**search_args)
-        }
-        attempts -= 1
+    results = {
+        ' '.join((
+            r.filename,
+            str(r.seeders) | green,
+            str(r.leechers) | yellow,
+            f"{r.size / 1024**3:.2f} GiB" | blue,
+            r.pubdate.split()[0] | magenta
+        )): r.download
+        for r in RarbgAPI(retries=3).search(**search_args)
+    }
     return results
 
 
 def choose_result(results):
     results.update({"Nothing": "Nothing"})
-    uri = choose("What should we get?", results, default="Nothing")
+    uri = choose("What should we get?" | magenta, results, default="Nothing")
     return None if uri == "Nothing" else uri
 
 
 def show_connection():
     try:
-        local['mullvad']['status', '-l'] &FG
+        print(local['mullvad']('status', '-l') | yellow)
     except CommandNotFound:
         try:
-            (local['nmcli']['-o'] | local['grep']['-E', '^[^ ]+: connected']) &FG
+            print((local['nmcli']['-o'] | grep['-E', '^[^ ]+: connected'])() | yellow)
         except CommandNotFound:
             try:
-                local['https']['--body', 'ipinfo.io'] &FG
+                print(local['https']('--body', 'ipinfo.io') | yellow)
             except CommandNotFound:
                 try:
-                    local['curl']['ipinfo.io'] &FG
+                    print(local['curl']('ipinfo.io') | yellow)
                 except CommandNotFound:
-                    local['wget']['-qO', '-', 'ipinfo.io'] &FG
+                    print(local['wget']('-qO', '-', 'ipinfo.io') | yellow)
 
 
 def main():
@@ -76,6 +69,7 @@ def main():
     if not results:
         sys.exit(1)
 
+    print(f"{df('-h', '-P', '.').splitlines()[-1].split()[3]} available" | yellow)
     uri = choose_result(results)
     if uri is None:
         sys.exit()
@@ -85,10 +79,12 @@ def main():
             local['xclip']['-sel', 'clip'] > sys.stdout
         ) >= sys.stderr) << uri)()
     except CommandNotFound:
-        print(uri)
+        print(uri | blue)
+    else:
+        print("Magnet URI copied to clipboard" | green)
 
     show_connection()
-    if ask("Begin download with aria2?", True):
+    if ask("Begin download with aria2" | magenta, True):
         aria2c['--seed-time=0', uri] &FG
 
 

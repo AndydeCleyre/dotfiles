d62709ece560b55060351d63a0ce6a7cdf081b37
Andy Kluger <andykluger@gmail.com>
2020-08-21 02:07:36 -0400

    minor improvements to readability of extraction script

--- Code/x.py
+++ Code/x.py
@@ -1,3 +1,5 @@
+#!/home/andy/.local/bin/vpy
+#!/home/andy/bin/vpy
 #!/usr/bin/env python3
 import sys
 from itertools import count
@@ -7,18 +9,20 @@ from plumbum import local, FG, CommandNotFound
 
 
 def extract(archive):
-    new_folder = local.path(archive.rsplit('.', 1)[0])
+    first_choice = local.path(archive).with_suffix('')
+    new_folder = first_choice
     c = count()
     while new_folder.exists():
-        new_folder = local.path(f'{new_folder}.{next(c)}')
+        new_folder = local.path(f'{first_choice}.{next(c)}')
     new_folder.mkdir()
     local['7z']['x', f'-o{new_folder}', archive] & FG
+    print(f"Destination: {new_folder}")
 
 
 if __name__ == '__main__':
     for arg in sys.argv[1:]:
         with suppress(CommandNotFound):
-            local['notify-send']('-i', '/usr/share/icons/Papirus/64x64/apps/ark.svg', 'Extracting . . .', arg)
+            local['notify-send']('-i', 'ark', 'Extracting . . .', arg)
         extract(arg)
         with suppress(CommandNotFound):
-            local['notify-send']('-i', '/usr/share/icons/Papirus/64x64/apps/ark.svg', 'Finished extracting!', arg)
+            local['notify-send']('-i', 'ark', 'Finished extracting!', arg)

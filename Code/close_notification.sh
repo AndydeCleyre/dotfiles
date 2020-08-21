c9e1dfc766fe60720fe5ec13a1a3e1b0a103051e
Andy Kluger <andykluger@gmail.com>
2020-08-21 02:11:42 -0400

    location adjustment for cursor

--- Code/close_notification.sh
+++ Code/close_notification.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 x=$(xdotool getmouselocation | cut -f 1 -d ' ' | cut -f 2 -d ':')
 y=$(xdotool getmouselocation | cut -f 2 -d ' ' | cut -f 2 -d ':')
-xdotool mousemove 1750 95
+xdotool mousemove 1700 100
 xdotool click 1
 xdotool mousemove $x $y

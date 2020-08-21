2109cb73214987997fcc36343168dd56a270026d
Andy Kluger <andykluger@gmail.com>
2020-08-21 02:10:06 -0400

    optional size-sorting for playdelete

--- Code/playdelete.py
+++ Code/playdelete.py
@@ -1,12 +1,16 @@
+#!/home/andy/.local/share/venvs/126dae705d4842d6e371dc55f7e7f923/venv/bin/python
+#!/home/andy/.local/bin/vpy
+#!/home/andy/bin/vpy
 #!/usr/bin/env python3
 import sys
 from random import shuffle
 import shutil
 
-from plumbum import local, ProcessExecutionError
-from plumbum.cmd import mpv
+from plumbum import local, ProcessExecutionError, FG
+from plumbum.cli import Application, SwitchAttr, Flag, Set
 from plumbum.cli.terminal import choose
-from plumbum.cli import Application, SwitchAttr
+from plumbum.colors import red, green
+from plumbum.cmd import mpv
 
 
 class PlayDeleter(Application):
@@ -22,54 +26,95 @@ class PlayDeleter(Application):
         default=0
     )
 
+    # biggest_first = Flag(
+        # ['b', 'biggest-first'],
+        # help="Play matching videos in order from biggest to smallest, instead of randomly"
+    # )
+
+    start = SwitchAttr(
+        ['s', 'sort', 'start'], Set('big', 'new'),
+        help="Sort by property, descending"
+    )
+
     def select_vids(self, paths, exts=('mkv', 'mp4', 'flv', 'avi', 'm4v', 'wmv')):
         vids = []
         for path in paths:
             root = local.path(path)
-            dir_filter = lambda d: len(root - d) < self.depth if self.depth else lambda d: True
-            vid_filter = lambda f: f.lower().rsplit('.', 1)[-1] in exts and ((f.stat().st_size / 2 ** 20 >= self.min_mebs) if self.min_mebs else True)
-            vids.extend(root.walk(vid_filter, dir_filter))
-        return vids
+
+            def dir_filter(d):
+                return len(d - root) < self.depth if self.depth else True
+
+            def vid_filter(f):
+                return f.suffix.lower().lstrip('.') in exts and (
+                    f.stat().st_size / 2 ** 20 >= self.min_mebs
+                    if self.min_mebs else True
+                )
+
+            if root.is_dir():
+                vids.extend(root.walk(vid_filter, dir_filter))
+            elif vid_filter(root):
+                vids.append(root)
+        return list(set(vids))
 
     def act(self, vid):
         action = "replay"
         while action == "replay":
             try:
-                mpv(vid)
+                mpv['--really-quiet', vid] & FG
             except ProcessExecutionError as e:
-                print(e, file=sys.stderr)
+                print(e | red, file=sys.stderr)
                 break
             try:
                 action = choose(
-                    f"\n{vid}:\n\n",
-                    ["let it be", "quit", "kill it dead", "replay", "move to . . ."],
+                    f"\n{vid} ({vid.stat().st_size / 2 ** 20:.2f} MiB):" | green,
+                    [
+                        "let it be",
+                        "quit",
+                        "kill it dead",
+                        "replay",
+                        "move to . . ."
+                    ],
                     "let it be"
                 )
             except KeyboardInterrupt:
-                sys.exit(0)
+                action = "quit"
         if action == "quit":
             sys.exit(0)
         elif action == "kill it dead":
             vid.delete()
-            print("SPLAT!")
+            print("SPLAT!" | green)
         elif action == "move to . . .":
             while True:
-                dest = local.path(input("Destination folder: "))
+                dest = local.path(input(f"Destination folder: {local.cwd}/"))
                 if not dest.is_dir():
-                    print(dest, "not found.")
+                    print(f"{dest} not found." | red)
+                elif (dest / vid.name).exists():
+                    print(f"{dest / vid.name} already exists!" | red, file=sys.stderr)
                 else:
                     try:
                         vid.move(local.path(dest))
                     except shutil.Error as e:
-                        print(e)
+                        print(e | red, file=sys.stderr)
                         continue
-                    print("WHOOSH!")
-                    break
+                    else:
+                        print("WHOOSH!" | green)
+                        break
 
     def main(self, *paths):
         vids = self.select_vids(paths)
-        shuffle(vids)
+        # if self.biggest_first:
+        if self.start:
+            vids.sort(
+                key=lambda v: (
+                    v.stat().st_size if self.start == 'big'
+                    else v.stat().st_mtime
+                ), reverse=True
+            )
+        else:
+            shuffle(vids)
         for vid in vids:
+            line = '-' * len(vid)
+            print(f"{line}\n{vid}\n{line}" | green)
             self.act(vid)
 
 

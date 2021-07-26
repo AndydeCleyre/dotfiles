96b6e8836cc59c3c2007830c6c1e1de2d711973e
Andy Kluger <andykluger@gmail.com>
2021-07-26 11:55:20 -0400

    small browser.py updates and include general reqs files

--- Code/browser.py
+++ Code/browser.py
@@ -1,5 +1,4 @@
 #!/home/andy/.local/bin/vpy
-#!/home/andy/bin/vpy
 #!/usr/bin/env python3
 import os
 import re
@@ -18,12 +17,13 @@ from plumbum.cmd import (
     notify_send,
     ps,
     qview,
-    tuir,
+    telegram_desktop,
+    ttrv,
     w3m,
     wget,
     xclip,
 )
-from requests import get
+from httpx import get
 
 
 def get_ice_cmd(appname: str) -> LocalCommand:
@@ -57,17 +57,25 @@ class Browser:
     run: Callable[[str], Any]
 
 
+tdesktop = Browser(
+    "Opening Telegram . . .",
+    lambda uri: uri.startswith('tg://'),
+    lambda uri: telegram_desktop['--', uri] & BG(stderr=sys.stderr)
+)
+
+        # r'(.*\.torrent$)'
+        # r'|(magnet:\?xt=urn:btih:)'
 streamer = Browser(
     "Attempting to stream . . .",
     lambda uri: re.match(
-        r'(.*\.torrent$)'
-        r'|(magnet:\?xt=urn:btih:)'
+        r'(https?://youtu\.be/)'
         r'|(https?://(www\.)?(m\.)?(youtube|vrv)\.com?/(watch|embed))'
-        r'|(https?://youtu\.be/)'
+        r'|(https?://vm\.tiktok\.com/)'
         r'|(https?://vimeo\.com/\d+)'
         r'|(https?://([^/]+\.)?gfycat\.com/.)'
         r'|(https?://i\.imgur\.com/.*(v\.jpg|\.mp4|\.gifv)$)'
-        r'|(https://v\.redd\.it/)', uri
+        r'|(https://v\.redd\.it/)',
+        uri
     ),
     local['~/Code/stream.sh']
 )
@@ -78,7 +86,8 @@ img_viewer = Browser(
     lambda uri: re.match(
         r'(.*\.(jp|pn)g$)'
         r'|(https://i\.imgur\.com/)'
-        r'|(https://i\.redd\.it/)', uri
+        r'|(https://i\.redd\.it/)',
+        uri
     ),
     fetch_and_view
 )
@@ -109,17 +118,18 @@ gphotos = Browser(
     "Opening photos . . .",
     lambda uri: re.match(
         r'(https://photos\.google\.com/)'
-        r'|(https://photos\.app\.goo\.gl/)', uri
+        r'|(https://photos\.app\.goo\.gl/)',
+        uri
     ),
     lambda uri: get_ice_cmd('googlephotos')[uri] & BG(stderr=sys.stderr)
 )
 
 
 ttyreddit = Browser(
-    "Using tuir in existing TTY . . .",
+    "Using ttrv in existing TTY . . .",
     lambda uri: uri.startswith('https://www.reddit.com/r/') and sys.stdin.isatty(),
     lambda uri: (
-        tuir[
+        ttrv[
             ['-s', [*filter(None, uri.split('/'))][-1]]
             if re.match(r'https://www\.reddit\.com/r/[^/]+/?$', uri)
             else uri
@@ -138,16 +148,17 @@ ttyweb = Browser(
         )
     ),
     # lambda uri: lynx['-accept_all_cookies', uri] & FG
-    lambda uri: w3m[uri] & FG
+    lambda uri: w3m[uri] &FG
+    # lambda uri: w3m['-o', 'confirm_qq=false', uri] &FG
 )
 
 
-def route(*uris: str):
+def route(*uris: str, debug=False):
     for uri in uris:
         uri = re.sub(r'^browser:/{0,2}', '', uri)
-
-        # streamer.run(uri)
-        # continue
+        uri = re.sub(r'^https://preview\.redd\.it/([^.]+\.png)(\?.*)', r'https://i.redd.it/\1', uri)
+        uri = re.sub(r'^https://t\.me/([^/]+)$', r'tg://resolve?domain=\1', uri)
+        uri = re.sub(r'^https://t\.me/([^/]+)/([^/]+)$', r'tg://\1?slug=\2', uri)
 
         for browser in (
             streamer,
@@ -156,21 +167,35 @@ def route(*uris: str):
             gmaps,
             gphotos,
             ttyreddit,
-            ttyweb,
             gcalendar,
+            tdesktop,
+
+            ttyweb,
         ):
+            if debug:
+                print(f"Checking {browser}")
+                print(browser.match(uri))
+                continue
             # notify_send('-a', "browser.py debugging", '_'+ps('ocmd=', os.getppid()), os.getppid())
-            # notify_send('-a', "browser.py debugging", "Checking for match:", browser)
             if browser.match(uri):
                 notify_send('-t', 1000, '-a', "browser.py", browser.notice, uri)
                 browser.run(uri)
                 break
         else:
+            if debug:
+                print("No match")
+                return
+            # TODO: maybe move ttyweb matching down here
+            # or rather: add a catch-all firefox Browser above, removing below:
             notify_send('-t', 1000, '-a', "browser.py", "Using Firefox . . .", uri)
-            with local.env(GTK_USE_PORTAL=1):
-                firefox[uri] & BG(stderr=sys.stderr)
+            firefox[uri] & BG(stderr=sys.stderr)
 
 
 if __name__ == '__main__':
+    if '--debug' in sys.argv:
+        sys.argv.remove('--debug')
+        debug=True
+    else:
+        debug=False
     uris = sys.argv[1:] or xclip('-sel', 'clip', '-o').splitlines()
-    route(*uris)
+    route(*uris, debug=debug)

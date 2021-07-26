e998fbc61ada694aaf642db2e9fd9a60841d4b58
Andy Kluger <andykluger@gmail.com>
2021-07-26 11:41:42 -0400

    latest version of github panel item, even though not currently using it

--- Code/panel_scripts/github/notifications.zsh
+++ Code/panel_scripts/github/notifications.zsh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 #      
-if [[ $1 == --click ]]; then
+here="$0:P:h"
+alert_color=$(python3 -c "print(''.join(hex(int(i))[2:] for i in '$(kreadconfig5 --group Colors:Button --key ForegroundActive)'.split(',')))")
+# yaml-merge -m entries =(<<<'{"entries": []}') <sample.json
+# data=$(gh api notifications | yaml-merge -m entries =(<<<'{"entries": []}'))
+
+if [[ $1 == --click ]] {
     messages=(${(s:\n:)"$(
         gh api notifications \
         | jello -r '"\n".join(n["repository"]["name"] + ": " + n["subject"]["title"] for n in _)'
@@ -11,15 +16,15 @@ if [[ $1 == --click ]]; then
         "Notifications: ${#messages}" "$body" \
         --action default:Go
     )"})
-    if [[ $resp[1] == default ]]; then
-        xdg-open 'https://github.com/notifications?query=is%3Aunread'
-    fi
-else
+    if [[ $resp[1] == default ]] xdg-open 'https://github.com/notifications?query=is%3Aunread'
+} else {
+    # tmpl=('@require(entries)' '@(count = len(entries))@count')
     # message_count=$(gh api notifications | jq length)
     message_count=$(gh api notifications | jello 'len(_)')
-    if (( message_count > 0 )); then
-        print -r '<font color="#709d3d"></font>'
-    else
-        print -r '<font color="#c2b790"></font>'
-    fi
-fi
+    # message_count=$(wheezy.template $here/count.wz gh api notifications | jello 'len(_)')
+    if (( message_count > 0 )) {
+        print -r "<font color=\"#${alert_color}\"></font>"
+    } else {
+        print -r ''
+    }
+}

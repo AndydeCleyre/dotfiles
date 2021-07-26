6ecd1ef5ba667848bd4415820db561e4a2a94265
Andy Kluger <andykluger@gmail.com>
2021-07-26 11:48:30 -0400

    update weather applet

--- Code/panel_scripts/weather/weather.py
+++ Code/panel_scripts/weather/weather.py
@@ -1,15 +1,13 @@
 #!/home/andy/.local/bin/vpy
 #!/usr/bin/env python3
 import sys
-from time import sleep
 from contextlib import suppress
+from time import sleep
 
-from plumbum import local, CommandNotFound, ProcessExecutionError
-from plumbum.cmd import notify_send
 import httpx
-
+from plumbum import CommandNotFound, ProcessExecutionError, local
+from plumbum.cmd import notify_send
 from vault import DARKSKY_API_KEY, LAT, LONG
-
 
 API_BASE = f'https://api.darksky.net/forecast/{DARKSKY_API_KEY}/{LAT},{LONG}'
 ICONS = {
@@ -46,12 +44,12 @@ def notify():
         notify_send('-a', f"Weather {title_icon}", summary, body)
 
 
-def display():
+def display(pre_pad='', post_pad=''):
     try:
         with suppress(CommandNotFound):
             local['nm-online']('-x')
     except ProcessExecutionError:
-        print("")
+        print(f"{pre_pad}{post_pad}")
     else:
         for attempt in range(3):
             try:
@@ -60,25 +58,31 @@ def display():
                     'units': 'uk2'
                 })
             except httpx.exceptions.NetworkError as e:
-                # notify_send('-a', "Weather", '-t', 30000, "Network Error", e)
-                notify_send('-a', "Weather", "Network Error", e)
-                # notify_send('-a', "Weather", "Network Error")
+                # notify_send('-a', "Weather", "Network Error", e)
                 sleep(2)
             else:
                 current = r.json()['currently']
                 temp = round(current['temperature'])
                 icon = ICONS[current['icon']]
-                print(f"{temp}°{icon}")
+                # print(f"{pre_pad}{temp}°{icon}{post_pad}")
+                # print(f"{pre_pad}{icon} {temp}°{post_pad}")
+                # print(f"{pre_pad}{icon}{temp}°{post_pad}")
+                print(f"{pre_pad}{icon} {temp}°{post_pad}")
                 break
         else:
-            print("")
+            print(f"{pre_pad}{post_pad}")
 
 
 if __name__ == '__main__':
-    # if '--debug' not in sys.argv[1:]:
-        # sleep(30)
-        # sys.exit()
     if '--click' in sys.argv[1:]:
         notify()
     else:
-        display()
+        pre_pad = (
+            '' if '--pre-pad' not in sys.argv[1:]
+            else sys.argv[sys.argv.index('--pre-pad', 1) + 1]
+        )
+        post_pad = (
+            '' if '--post-pad' not in sys.argv[1:]
+            else sys.argv[sys.argv.index('--post-pad', 1) + 1]
+        )
+        display(pre_pad=pre_pad, post_pad=post_pad)

b851ac8bcb48c1a482d8977333a74ee2cfca0874
Andy Kluger <andykluger@gmail.com>
2021-07-26 11:42:43 -0400

    small update mullvad panel item

--- Code/panel_scripts/mullvad/connected.zsh
+++ Code/panel_scripts/mullvad/connected.zsh
@@ -1,23 +1,22 @@
 #!/bin/zsh
+# connected_color=$(python3 -c "print(''.join(hex(int(i))[2:] for i in '$(kreadconfig5 --group Colors:Button --key ForegroundPositive)'.split(',')))")
+connected_color=$(python3 -c "print(''.join(hex(int(i))[2:] for i in '$(kreadconfig5 --group Colors:Button --key ForegroundActive)'.split(',')))")
 mullvad_status () {
     local mullvad_status=$(mullvad status)
     local is_connected=
-    if [[ $mullvad_status =~ '.*: Connected to WireGuard .*' ]]; then
-        is_connected=1
-    fi
-    if [[ $1 == --click ]]; then
-        if [[ $is_connected ]]; then
+    if [[ $mullvad_status =~ '.*: Connected to (WireGuard|OpenVPN) .*' ]] is_connected=1
+    if [[ $1 == --click ]] {
+        if [[ $is_connected ]] {
             mullvad disconnect
-        else
+        } else {
             mullvad connect
-        fi
+        }
         sleep 1
         notify-send -a Mullvad -i mullvad-vpn "VPN Connection" "$(mullvad status -l)"
-    elif [[ $is_connected ]]; then
-        print '<font color="#709d3d"></font>'
-    else
-        # print '<font color="#e64976"></font>'
+    } elif [[ $is_connected ]] {
+        print "<font color=\"#${connected_color}\"></font>"
+    } else {
         print ''
-    fi
+    }
 }
 mullvad_status $@

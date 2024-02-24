254766f04dbed152dbe8f33196a841d329386f14
Andy Kluger <AndyKluger@gmail.com>
2024-02-24 14:44:41 -0500

    Wezterm config updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,38 +5,46 @@ local wezterm = require 'wezterm'
 -- local scheme = wezterm.get_builtin_color_schemes()["kanagawabones"]
 -- local scheme = wezterm.get_builtin_color_schemes()["Popping and Locking"]
 -- local scheme = wezterm.get_builtin_color_schemes()["Doom Peacock"]
-local scheme = wezterm.get_builtin_color_schemes()["Red Planet"]
+-- local scheme = wezterm.get_builtin_color_schemes()["Red Planet"]
+local scheme = wezterm.get_builtin_color_schemes()["One Dark (Gogh)"]
 
 scheme.background = "#1f1f28"
 scheme.ansi[8] = "#dea050"
 scheme.brights[8] = "#de9231"
 
+-- No effect?
+scheme.cursor_fg = "#8080c6"
+
 return {
   color_schemes = { ["ConfiguredColors"] = scheme },
   color_scheme = "ConfiguredColors",
-  window_background_opacity = 0.95,
+  -- window_background_opacity = 0.95,
 
   font_size = 24,
   font = wezterm.font_with_fallback({
     "Iosevka Term Custom",
+    "0xProto",
+    "Sudo",
     "Fantasque Sans Mono",
+    "Ubuntu Mono",
+    "Nimbus Mono PS",
     "Symbols Nerd Font",
-    "Untitled1",
     "NanumGothicCoding",
-    "OpenMoji"
+    "OpenMoji",
+    "JoyPixels",
+    "Untitled1"
   }),
-  harfbuzz_features = { 'thnd', 'frac' },
+  harfbuzz_features = { 'kern', 'liga', 'clig', 'thnd', 'frac', 'txtr' },
 
   disable_default_key_bindings = true,
   keys = {
-    { key = '=', mods = 'CTRL',       action = wezterm.action.IncreaseFontSize },
-    { key = '-', mods = 'CTRL',       action = wezterm.action.DecreaseFontSize },
-    { key = '0', mods = 'CTRL',       action = wezterm.action.ResetFontSize },
+    { key = '=', mods = 'CTRL',       action = wezterm.action.IncreaseFontSize      },
+    { key = '-', mods = 'CTRL',       action = wezterm.action.DecreaseFontSize      },
+    { key = '0', mods = 'CTRL',       action = wezterm.action.ResetFontSize         },
     { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
   },
 
   animation_fps = 30,
-  -- default_cursor_style = "BlinkingBar",
   default_cursor_style = "SteadyBar",
   canonicalize_pasted_newlines = "LineFeed",
   enable_tab_bar = false,

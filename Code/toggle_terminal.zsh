6b301ef750cae83f8d2e60e4e9df93770226f865
Andy Kluger <AndyKluger@gmail.com>
2025-03-01 01:49:38 -0500

    return to ghostty for now

--- Code/toggle_terminal.zsh
+++ Code/toggle_terminal.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-${0:P:h}/toggle_window.zsh wezterm-gui org.wezfurlong.wezterm
-# ${0:P:h}/toggle_window.zsh ghostty
+# ${0:P:h}/toggle_window.zsh wezterm-gui org.wezfurlong.wezterm
+${0:P:h}/toggle_window.zsh ghostty
